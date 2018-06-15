8632e6e1fa24a879015394baec1ae6fa119914b4
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-06-15 11:40:36 +0300

    code reivew

--- dev-zheng/assignment1/src/App.js
+++ dev-zheng/assignment1/src/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TimeLine from './components/timeline.jsx';
+import React, {
+  Component
+} from 'react';
+import logo       from './logo.svg';
+
+import TimeLine   from './components/timeline.jsx';
 import EventGroup from './components/eventGroup.jsx';
-import Modal from 'react-responsive-modal';
+import Modal      from 'react-responsive-modal';
+
+import './App.css';
 
 const timelines = ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00']
 const events = [
